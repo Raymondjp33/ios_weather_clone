bf74a126b3dc5b4ade6126e4f04cb8e42df507e8
Raymond Price <RaymondJPrice33@gmail.com>
2024-09-06 16:41:21 -0400

    Renamed list_page to list_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:lyssn_project/list_page.dart';
+import 'package:lyssn_project/list_widget.dart';
 
 void main() {
   runApp(const MyApp());
