33f6505c0010172eaa27f41bb3b145cc03b5153d
Raymond Price <RaymondJPrice33@gmail.com>
2024-09-24 06:49:05 -0400

    Weather provider edited to match new models

--- lib/providers/weather_provider.dart
+++ lib/providers/weather_provider.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import '../models/forecast.model.dart';
+import '../models/weather_response.model.dart';
 
 class WeatherProvider extends ChangeNotifier {
   String forecastEndpoint(String zip) =>
       'https://api.weatherapi.com/v1/forecast.json?key=78b69989d8d145528da153537241309&q=$zip&days=1';
   String zipCode = '';
 
-  ForecastModel? forecastModel;
+  WeatherModel? weatherModel;
 
   void setZipcode(String value) {
     zipCode = value;
@@ -18,13 +18,10 @@ class WeatherProvider extends ChangeNotifier {
   }
 
   Future<void> callWeatherApi() async {
-    var forecast = json
+    Map<String, dynamic> forecast = json
         .decode((await http.get(Uri.parse(forecastEndpoint(zipCode)))).body);
 
-    forecastModel = ForecastModel(
-      location: forecast['location'],
-      current: forecast['current'],
-    );
+    weatherModel = WeatherModel.fromJson(forecast);
 
     notifyListeners();
   }
